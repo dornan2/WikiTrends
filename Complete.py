375fa07f4a991857c4cf57aaf4c550bb61350b80
Adrian Dornan <adrian.dornan2@mail.dcu.ie>
2015-03-26 23:23:06 +0000

    fixed encoding

--- Complete.py
+++ Complete.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from datetime import datetime
 from pymongo import MongoClient
+import urllib
 import gzip
 import io
 import os
@@ -19,6 +20,29 @@ year = "15"
 month = "03"
 day = "21"
 hour = "0"
+
+client = MongoClient('localhost', 27017)
+
+db = client.wiki_database
+collection = db.article_hits
+
+days_views = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0}
+
+yearly_views = {
+     '1': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
+     '2': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
+     '3': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0},
+     '4': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
+     '5': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
+     '6': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
+     '7': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
+     '8': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
+     '9': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
+    '10': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
+    '11': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
+    '12': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0}
+}
+
 
 while int(hour) < 1:
     # url = "http://dumps.wikimedia.org/other/pagecounts-raw/" + year + "/" + year + "-" + month + "/pagecounts-" + year + month + day + "-" + hour + "0000.gz"
@@ -54,92 +78,63 @@ while int(hour) < 1:
         io.open('C:\\Users\\Adrian\\FYP\\' + file_name + '.txt','w',encoding='utf-8') as outfile:
         for line in infile:
             if (line.startswith(("EN ", "En ", "en ")) and
-                     not line.startswith(("en Category:", "en Wikipedia%3","Help:", "en Portal_talk:", "en Talk%3A", "en Template_talk", "en Special%", "en Portal:", "en Template_talk:", "en Draft:", "en File:", "en Category%3A", "en Wiki/", "en Talk%3a", "File_talk:", "en Special:", "en Talk:", "en Template:", "en User:", "en Wikipedia:", "en Wikipedia_talk:", "en User_talk", "en Category_talk:", "en Search_search_", "en category:", "en en:", "en  ")) and
-                     all(c in string.printable for c in line) and
-                     len(line) < 120 and
-                     int(line.split()[3])/int(line.split()[2]) > 6732 and
-                     "." not in line.split()[1]
+                    not line.startswith(("en Category:", "en Wikipedia%3","Help:", "en Portal_talk:", "en Talk%3A", "en Template_talk", "en Special%", "en Portal:", "en Template_talk:", "en Draft:", "en File:", "en Category%3A", "en Wiki/", "en Talk%3a", "File_talk:", "en Special:", "en Talk:", "en Template:", "en User:", "en Wikipedia:", "en Wikipedia_talk:", "en User_talk", "en Category_talk:", "en Search_search_", "en category:", "en en:", "en  ")) and
+                    all(c in string.printable for c in line) and
+                    len(line) < 120 and
+                    int(line.split()[3])/int(line.split()[2]) > 6732 and
+                    "." not in line.split()[1] and
+                    int(line.split()[2]) >= 3
             ):
-                outfile.write(line)
+                hits = int(line.split()[2])
+                article_Name = urllib.parse.unquote(line.split()[1] + ":" + str(year),encoding='utf-8')
 
-
-
-    client = MongoClient('localhost', 27017)
-
-    db = client.wiki_database
-    collection = db.article_hits
-
-    days_views = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0}
-
-    yearly_views = {
-         '1': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
-         '2': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
-         '3': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0},
-         '4': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
-         '5': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
-         '6': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
-         '7': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
-         '8': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
-         '9': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
-        '10': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0},
-        '11': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0},
-        '12': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0}
-      }
-
-    num = 1
-
-    with io.open('C:\\Users\\Adrian\\FYP\\sample1.txt','r',encoding='utf-8') as infile:
-        for line in infile:
-            hits = int(line.split()[2])
-            article_Name = line.split()[1] + ":" + str(year)
-
-            if collection.find({"_id": article_Name}, {"_id": 1}).limit(1).count() < 1:
-                collection.insert(
-                    {
-                        "_id": article_Name,
-                        "day_total": 0,
-                        "month_total": 0,
-                        "year_total": 0,
-                        "daily_views": days_views,
-                        "yearly_views": yearly_views
-                    }
-                )
-            #when it is a new month we need to find out what yesterdays date was
-            #if its a new day updates yearly views and resets day_total and daily views
-            if hour == 0 and day > 1:
-                collection.update(
-                    {'_id': article_Name},
-                    {'$set': {
-                        'yearly_views.'+str(month)+"."+str(day-1): collection.find_one({"_id": article_Name})['day_total'],
-                        'day_total': 0,  "daily_views": days_views
+                if collection.find({"_id": article_Name}, {"_id": 1}).limit(1).count() < 1:
+                    collection.insert(
+                        {
+                            "_id": article_Name,
+                            "day_total": 0,
+                            "month_total": 0,
+                            "year_total": 0,
+                            "daily_views": days_views,
+                            "yearly_views": yearly_views
                         }
-                    },
-                    True
-                )
-            else:
-                #if it is a new day of a new month update yearly views and reset day_total,daily views and monthly total
-                if hour == 0 and day == 0:
+                    )
+                #when it is a new month we need to find out what yesterdays date was
+                #if its a new day updates yearly views and resets day_total and daily views
+                if hour == 0 and day > 1:
                     collection.update(
                         {'_id': article_Name},
                         {'$set': {
-                            'yearly_views.'+str(month-1)+"."+str((date.today() - timedelta(1)).strftime('%d')): collection.find_one({"_id": article_Name})['day_total'],
-                            'day_total': 0, 'month_total': 0, "daily_views": days_views
+                            'yearly_views.'+str(month)+"."+str(day-1): collection.find_one({"_id": article_Name})['day_total'],
+                            'day_total': 0,  "daily_views": days_views
                             }
                         },
                         True
                     )
+                else:
+                    #if it is a new day of a new month update yearly views and reset day_total,daily views and monthly total
+                    if hour == 0 and day == 0:
+                        collection.update(
+                            {'_id': article_Name},
+                            {'$set': {
+                                'yearly_views.'+str(month-1)+"."+str((date.today() - timedelta(1)).strftime('%d')): collection.find_one({"_id": article_Name})['day_total'],
+                                'day_total': 0, 'month_total': 0, "daily_views": days_views
+                                }
+                            },
+                            True
+                        )
 
 
-            collection.update(
-                {'_id': article_Name},
-                {'$set': {'daily_views.' + str(hour): hits},
-                 '$inc': {'day_total': hits, 'month_total': hits, 'year_total': hits}
-                },
-                True
-            )
+                collection.update(
+                    {'_id': article_Name},
+                    {'$set': {'daily_views.' + str(hour): hits},
+                     '$inc': {'day_total': hits, 'month_total': hits, 'year_total': hits}
+                    },
+                    True
+                )
 
-    print("The database has been successfully updated")
-    hour = int(hour) + 1
+        print("The database has been successfully updated")
+        hour = int(hour) + 1
 
 #clean talk files
 print("Deleting old files...")
