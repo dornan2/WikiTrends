c47dbd58c1bae91ffef7403d2566b6fcb4feb7ae
Adrian Dornan <adrian.dornan2@mail.dcu.ie>
2015-05-26 23:20:23 +0100

    forecast api added

--- models/forecastModel.js
+++ models/forecastModel.js
@@ -5,8 +5,23 @@ var forecastModel = new Schema({
     _id: {type: Number},
     name: {type: String},
     total: {type: Number},
-    forecast: {type: Number}
+    forecast: {
+        1: Number,
+        2: Number,
+        3: Number,
+        4: Number,
+        5: Number,
+        6: Number,
+        7: Number,
+        8: Number,
+        9: Number,
+        10: Number,
+        11: Number,
+        12: Number,
+        13: Number,
+        14: Number
+    }
 });
 
-module.exports = mongoose.model('forecast', forecastModel);
+module.exports = mongoose.model('forecast100', forecastModel);
 
